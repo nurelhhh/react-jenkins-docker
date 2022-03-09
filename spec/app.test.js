1b428f0959fd286321305f1ef7a1a69e063f5db9
nurelhhh <nurel_harsya@outlook.com>
2022-03-09 11:24:51 +0700

    Change test script

--- spec/app.test.js
+++ spec/app.test.js
@@ -4,5 +4,5 @@ import App from './App';
 
 it('renders welcome message', () => {
   render(<App />);
-  expect(screen.getByText('Learn React')).toBeInTheDocument();
+  expect(screen.getByText('030')).toBeInTheDocument();
 });
